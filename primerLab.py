fb4a1c3355199c1527d01104ec3edfeefb8def08
John Praddo <jhoncitof1ea10@gmail.com>
2022-12-23 16:26:15 -0500

    operaciones basicas con condicionales en python

---
+++ primerLab.py
@@ -0,0 +1,17 @@
+num1 = int(input("ingrese un nu: "))
+num2 = int(input("ingrese un nu: "))
+
+if (num1 == 0) and (num2   ==0):
+    pass
+elif (num1 != 0) and (num2 !=0):
+    print("la suma es", num2 +num1)
+    pass
+
+for i in range(0,5):
+    print(i)
+
+n1 = 5
+while n1> 0:
+    n1 -= 1
+    print(n1)
+
