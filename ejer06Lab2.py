fca2901b2991de24f9589010293f7a61e175512e
John Praddo <jhoncitof1ea10@gmail.com>
2022-12-27 08:32:30 -0500

    comprobado las maneras de caminaar del robot

--- ejer06Lab2.py
+++ ejer06Lab2.py
@@ -7,7 +7,7 @@ def pasosRobot(n):
     return pasosRobot(n-1)+pasosRobot(n-2)
 print("El bot puede caminar de ", pasosRobot(4), "maneras diferentes")
 
-#si son 4 metros, existen estaas posibilidades 
+#si son 4 metros, existen estas posibilidades 
 #1ra. Se puede caminar siempre de 1m: 1-1-1-1
 #2da. se puede caminar 2 metros con pasos de 1m y lo restante con pasos de 2m : 1-1-2
 #3ra. 2metros con pasos de 2m, el restante con paasos de 1: 2-1-1
