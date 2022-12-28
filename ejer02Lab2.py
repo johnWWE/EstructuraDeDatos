3080ff89495333f5f3edd092a809020871dbfa98
John Praddo <jhoncitof1ea10@gmail.com>
2022-12-28 10:17:36 -0500

    ejecutamos variaas veces el algoritmo de la pregunt 1, y respondemos la pregunta asignada

---
+++ ejer02Lab2.py
@@ -0,0 +1,23 @@
+#Corra el algoritmo anterior “desordena” (del ejercicio 1) muchas veces para 
+# la misma sucesión de entrada. ¿Como puede analizarse la salida para determinar
+#  si es  verdaderamente “aleatorio”?
+
+
+import random
+def desordenar(lista):
+ #iteracion para la cantidad de valores que tendra la lista creada posteriormente
+    for i in range(len(lista)):
+        aleatorio = random.randint(0,len(lista)-1) 
+        listaTemporal = lista[i]   #creamos un variable para almacenar los valores de la lista enla iteracion
+        lista[i] = lista[aleatorio]  # solamente hacemos un intercambio 
+        lista[aleatorio] =listaTemporal
+    return lista
+lista = [1,2,3,4,5,6,7,8,9]
+print(desordenar(lista))
+
+'''
+evidentemente la respuesta lo podemos determinar ejecutando varias veces el algoritmo, y
+efectivamente salen desordenados las listas introducidas, cuando ejecute tres veces, 
+salian los sgtes, resultados
+
+'''
