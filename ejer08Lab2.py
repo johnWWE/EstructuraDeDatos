abf3050238720effdc1232085090a72227cd11a3
John Praddo <jhoncitof1ea10@gmail.com>
2022-12-30 11:08:50 -0500

    comentarios finales

--- ejer08Lab2.py
+++ ejer08Lab2.py
@@ -3,13 +3,13 @@
 # y 2n = 2(n-1)/2. 2(n-1)/2.2 si n es impar
 
 def potencia(base, exponente):
-    if exponente == 1:
-        return base
-    elif exponente ==0 and base !=0:
-        return 1
+    if exponente == 1:    #cualquier numero elevado a uno sigue siendo la misma
+        return base   
+    elif exponente ==0 and base !=0:   #todo numero eleevado a cero es 1,
+        return 1                        #pero la base diferente a cero sino seria indetermiado
     elif base==0 and exponente==0:
-        return 'Es indeterminado'
+        return 'Es indeterminado' 
     else:
         # utilizamos la funcion recursiva ya que es un paso iterativo
-        return base * potencia(base, exponente-1)
+        return base * potencia(base, exponente-1)   #esto es 2 + potencia(2,4) iterativamente hasta que las condiciones ya no se cumplan
 print(potencia(2,5))
